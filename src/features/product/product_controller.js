33d859c4e37cc318201b23e93311a8111ce0bb23
Kanhaiya kumar <kanhaiya@aetherss.com>
2023-11-10 22:06:52 +0530

    error handling in express

--- src/features/product/product_controller.js
+++ src/features/product/product_controller.js
@@ -1,18 +1,17 @@
-import ProductModel from './product_model.js';
+import ProductModel from "./product_model.js";
 export default class ProductController {
-  
   getAllProducts(req, res) {
     const products = ProductModel.GetAll();
-     return res.status(200).send(products);
+    return res.status(200).send(products);
   }
 
   addProduct(req, res) {
-    const {name, price, sizes } = req.body;
-    console.log(req.file)
+    const { name, price, sizes } = req.body;
+    console.log(req.file);
     const newProduct = {
       name,
       price: parseFloat(price),
-      sizes: sizes.split(','),
+      sizes: sizes.split(","),
       imageUrl: req.file.filename,
     };
     const createRecord = ProductModel.add(newProduct);
@@ -23,13 +22,12 @@ export default class ProductController {
     const userID = req.query.userID;
     const productID = req.query.productID;
     const rating = req.query.rating;
-
-    const error = ProductModel.rateProduct(userID, productID, rating);
-    if(error) {
-      return res.status(400).send(error);
-    } else {
-      return res.status(200).send('okay');
+    try {
+      ProductModel.rateProduct(userID, productID, rating);
+    } catch (error) {
+      return res.status(400).send(error.message);
     }
+    return res.status(200).send("okay");
   }
 
   getOneProduct(req, res) {
@@ -37,23 +35,20 @@ export default class ProductController {
     // console.log(id);
 
     const product = ProductModel.GetOneProduct(id);
-    if(product) {
+    if (product) {
       res.status(200).send(product);
     } else {
-      res.status(404).send('product not found');
+      res.status(404).send("product not found");
     }
   }
-
 
   filterProducts(req, res) {
     const minPrice = parseInt(req.query.minPrice);
     const maxPrice = parseInt(req.query.maxPrice);
-    const category = (req.query.category);
+    const category = req.query.category;
 
     const result = ProductModel.filter(minPrice, maxPrice, category);
 
     res.status(200).send(result);
   }
-
-
-};
+}
--- src/features/product/product_model.js
+++ src/features/product/product_model.js
@@ -1,81 +1,82 @@
-import UserModel from './../user/user_model.js';
+import UserModel from "./../user/user_model.js";
 export default class ProductModel {
-    constructor(id, name, desc, price, imageUrl, category, size){
-        this.id = id;
-        this.name = name;
-        this.desc = desc;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.category = category;
-        this.size = size;
+  constructor(id, name, desc, price, imageUrl, category, size) {
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.category = category;
+    this.size = size;
+  }
+
+  static GetOneProduct(id) {
+    const product = products.find((i) => i.id == id);
+    return product;
+  }
+
+  static GetAll() {
+    return products;
+  }
+
+  static add(product) {
+    product.id = products.length + 1;
+    products.push(product);
+    return product;
+  }
+
+  static rateProduct(userID, productID, rating) {
+    // 1. Validate user and product
+    const user = UserModel.getAll().find((u) => u.id == userID);
+    if (!user) {
+      // return 'User not found';
+      throw new Error("User not found");
+    }
+    // Validate Product
+    const product = products.find((p) => p.id == productID);
+    if (!product) {
+      // return 'Product not found';
+      throw new Error("Product not found");
     }
 
-    static GetOneProduct(id) {
-      const product = products.find(i => i.id == id);
-      return product;
-    }
-
-    static GetAll() {
-      return products;
-    }
-
-    static add(product) {
-      product.id = products.length + 1;
-      products.push(product);
-      return product;
-    }
-
-
-    static rateProduct(userID, productID, rating) {
-      // 1. Validate user and product
-      const user = UserModel.getAll().find((u) => u.id == userID);
-      if(!user){
-        return 'User not found';
-      }
-      // Validate Product
-      const product = products.find((p) => p.id == productID);
-      if(!product){
-        return 'Product not found';
-      }
-
-      // 2. check if there are any ratings and if not then add rating array
-      if(!product.ratings) {
-        product.ratings = [];
+    // 2. check if there are any ratings and if not then add rating array
+    if (!product.ratings) {
+      product.ratings = [];
+      product.ratings.push({
+        userID: userID,
+        rating: rating,
+      });
+    } else {
+      // check if user rating is already available
+      const existingRatingIndex = product.ratings.findIndex(
+        (r) => r.userID == userID
+      );
+      if (existingRatingIndex >= 0) {
+        product.ratings[existingRatingIndex] = {
+          userID: userID,
+          rating: rating,
+        };
+      } else {
+        // 4. if no existing rating, then add new rating
         product.ratings.push({
           userID: userID,
           rating: rating,
         });
-      } else {
-        // check if user rating is already available
-        const existingRatingIndex = product.ratings.findIndex((r) => r.userID == userID);
-        if(existingRatingIndex >= 0){
-          product.ratings[existingRatingIndex] = {
-            userID: userID,
-            rating: rating,
-          };
-        } else {
-          // 4. if no existing rating, then add new rating
-          product.ratings.push({
-            userID: userID,
-            rating: rating,
-          });
-        }
       }
     }
+  }
 
-    static filter(minPrice, maxPrice, category) {
-      const result = products.filter((product) => {
-        return (
-          (!minPrice || product.price >= minPrice) && 
-          (!maxPrice || product.price <= maxPrice) && 
-          (!category || product.category == category)
-        );
-      });
-      return result;
-    } 
-    
-    
-};
+  static filter(minPrice, maxPrice, category) {
+    const result = products.filter((product) => {
+      return (
+        (!minPrice || product.price >= minPrice) &&
+        (!maxPrice || product.price <= maxPrice) &&
+        (!category || product.category == category)
+      );
+    });
+    return result;
+  }
+}
 
 var products = [
   new ProductModel(
